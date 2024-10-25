d6c3bf7ee294c1104db28b18d3f4461bf402d3b7
Sudhir-tbk-9 <trunts.back9@gmail.com>
2024-10-25 12:37:32 +0530

    Punch bgChange along with floating BottomTab Nav

--- Component/Drawer/Drawer.tsx
+++ Component/Drawer/Drawer.tsx
@@ -4,19 +4,20 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import BottomTabNavigator from '../BottomNav/BottomNav'; 
 import CustomDrawerContent from './DrawerContnet'; // Import your custom content
 import BottomTabNavLM from '../BottomNav/BottomNavForLM';
+import { View } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   return (
-    <SafeAreaView style={{ flex: 1 }}> 
+    <View style={{ flex: 1 }}> 
       <Drawer.Navigator 
         initialRouteName='Attendance'
         drawerContent={(props) => <CustomDrawerContent {...props} />}>
         <Drawer.Screen  name="Attendance" component={BottomTabNavigator} /> 
         <Drawer.Screen  name="LeaveManagement" component={BottomTabNavLM} /> 
       </Drawer.Navigator>
-    </SafeAreaView>
+    </View>
   );
 }
 
